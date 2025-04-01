08ac1e214febf2118b29ce60e98c8c9dd1c2d6e2
eliransher <scherzter@gmail.com>
2025-04-01 11:58:45 +0300

    general experiment

--- optimize/general_slurm.sh
+++ optimize/general_slurm.sh
@@ -3,4 +3,4 @@
 #SBATCH -A def-dkrass
 #SBATCH --mem 20000
 source /home/eliransc/projects/def-dkrass/eliransc/queues/bin/activate
-python /home/eliransc/projects/def-dkrass/eliransc/non_renewal_queueing_system/code/batch_data_depart_1.py
+python /home/eliransc/projects/def-dkrass/eliransc/one.deep.moment/optimize/multi_general.py
